5cd42db0e58d5b2f351b4396686717afabcc17f3
John Nyingi <JNyingi@resolution.lan>
2021-08-13 22:30:18 +0300

    Init EntryLog

---
+++ EntryLog.Extensions.Logging/IEntryLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntryLog.Extensions.Logging
+{
+    public interface IEntryLog
+    {
+        void Log(string folder, Exception exception);
+        void Log(string folder, string message);
+    }
+}
