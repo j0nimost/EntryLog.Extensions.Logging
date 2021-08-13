26ecfd5e1de98dca0e7786ef50877002b4609d8c
John Nyingi <JNyingi@resolution.lan>
2021-08-13 22:29:16 +0300

    Added an Extensible Config

---
+++ EntryLog.Extensions.Logging/EntryLogConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace EntryLog.Extensions.Logging
+{
+    public enum LogInterval
+    {
+        EveryDay = 0,
+        EveryHour = 1,
+        EveryMinute = 2,
+        EverySecond = 3
+    }
+    public enum LogType
+    {
+        Audit,
+        Warning,
+        Error
+    }
+    public class EntryLogConfiguration
+    {
+        public int EventId { get; set; }
+        public LogInterval LogInterval { get; set; }
+        public Uri FolderPath { get; set; }
+        public bool ShowOnlyMessagesOnError { get; set; }
+
+        public Dictionary<LogLevel, LogType> LogTypes = new Dictionary<LogLevel, LogType>()
+        {
+            {LogLevel.Information, LogType.Audit },
+            {LogLevel.Warning, LogType.Warning },
+            {LogLevel.Error, LogType.Error }
+        };
+
+        // TODO: Provide a means to create custom folder names and write to them
+    }
+}
