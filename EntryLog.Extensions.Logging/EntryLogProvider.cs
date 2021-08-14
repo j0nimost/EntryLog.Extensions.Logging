11dad9f9679cb4ddcfdd97a1130ff73a715516bf
John Nyingi <JNyingi@resolution.lan>
2021-08-15 00:17:06 +0300

    Added provider instantiater

--- EntryLog.Extensions.Logging/EntryLogProvider.cs
+++ EntryLog.Extensions.Logging/EntryLogProvider.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,15 +9,26 @@ namespace EntryLog.Extensions.Logging
 {
     public class EntryLogProvider : ILoggerProvider
     {
+        private readonly IDisposable _onChangeToken;
+        private EntryLogConfiguration _config;
+        private readonly ConcurrentDictionary<string, EntryLogger> _logger = new ConcurrentDictionary<string, EntryLogger>();
+        public EntryLogProvider(IOptionsMonitor<EntryLogConfiguration> config)
+        {
+            _config = config.CurrentValue;
+            _onChangeToken = config.OnChange(updateConfig => _config = updateConfig);
+        }
+
+        private EntryLogConfiguration GetEntryLogConfig() => _config;
         public ILogger CreateLogger(string categoryName)
         {
             // Create an instance for the EntryLog
-            return null;
+            return _logger.GetOrAdd(categoryName, name => new EntryLogger(name, GetEntryLogConfig));
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _logger.Clear();
+            _onChangeToken.Dispose();
         }
     }
 }
