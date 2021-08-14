9c0f3c1460b80664d25d8ff3bcd18f9ee077f95c
John Nyingi <JNyingi@resolution.lan>
2021-08-14 15:26:30 +0300

    New Implimentations: ToDO

---
+++ EntryLog.Extensions.Logging/EntryLogProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntryLog.Extensions.Logging
+{
+    public class EntryLogProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            // Create an instance for the EntryLog
+            return null;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
